86159e8b3fad1b4a14e7072fb2cd489935eca0e1
Дарья Кушнарева DJS-11/24 <dariyaaa.ku@gmail.com>
2025-02-23 10:31:13 +0300

    Update code.js

--- code.js
+++ code.js
@@ -1,4 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("DOM fully loaded and parsed");
   const apiKey='c3bee3db166e4b62bed70937252302';
+
+  const fetchData = (cityName)=> {
+try {
+  const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityName}&aqi=no&lang=ru`);
+                               console.log(response);
+} catch (error) {
+  console.log(error)
+}
+}
+  fetchData('Moscow');
+  fetchData('Sochi');
+  fetchData('Yaroslavl');
 });
